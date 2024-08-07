97578abc75e9a9196da11269bbabf61db72fa8e4
hamilton <hamilton-souza@outlook.com.br>
2024-08-07 17:51:24 -0300

    feat(component) : criado componente de botao junto dos presets primary e outline

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import {TouchableOpacityBox, TouchableOpacityBoxProps} from '../Box/Box';
+import {buttonPresets} from './buttonPresets';
+import {ActivityIndicator} from '../ActivityIndicator/ActivityIndicator';
+import {Text} from '../Text/Text';
+
+export type ButtonPreset = 'primary' | 'outline';
+
+export interface ButtonProps extends TouchableOpacityBoxProps {
+  title: string;
+  loading?: boolean;
+  preset?: ButtonPreset;
+  disabled?: boolean;
+}
+
+export function Button({
+  title,
+  loading,
+  disabled,
+  preset = 'primary',
+  ...touchableOpacityBoxProps
+}: ButtonProps) {
+  const buttonPreset = buttonPresets[preset][disabled ? 'disabled' : 'default'];
+
+  return (
+    <TouchableOpacityBox
+      disabled={disabled || loading}
+      paddingHorizontal="s20"
+      height={50}
+      alignItems="center"
+      justifyContent="center"
+      borderRadius="s16"
+      {...touchableOpacityBoxProps}
+      {...buttonPreset.container}>
+      {loading ? (
+        <ActivityIndicator color={buttonPreset.content} />
+      ) : (
+        <Text preset="paragraphMedium" bold color={buttonPreset.content}>
+          {title}
+        </Text>
+      )}
+    </TouchableOpacityBox>
+  );
+}
