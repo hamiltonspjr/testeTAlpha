03347d499bdc5b4dd866297ae0afb9a3760e889a
hamilton <hamilton-souza@outlook.com.br>
2024-08-08 17:32:12 -0300

    feat(auth): arquivo da camada de aplicação da arquitetura que consiste no caso de uso de logar no app

---
+++ src/domain/Auth/useCases/useAuthSignIn.ts
@@ -0,0 +1,33 @@
+import {useMutation} from '@tanstack/react-query';
+import {AuthCredentials} from '../authTypes';
+import {authService} from '../authService';
+import {MutationOptions} from '../../../infra/infraTypes';
+import {useAuthCredentials} from '../../../services/authCredentials/useAuthCredentials';
+
+interface Variables {
+  taxNumber: string;
+  password: string;
+}
+
+export function useAuthSignIn(options?: MutationOptions<AuthCredentials>) {
+  const {saveCredentials} = useAuthCredentials();
+  const mutation = useMutation<AuthCredentials, Error, Variables>({
+    mutationFn: ({taxNumber, password}) =>
+      authService.signIn(taxNumber, password),
+    retry: false,
+    onError: error => {
+      if (options?.onError) {
+        console.log(error);
+        options.onError(error.message);
+      }
+    },
+    onSuccess: authCredentials => {
+      saveCredentials(authCredentials);
+    },
+  });
+
+  return {
+    isLoading: mutation.isPending,
+    signIn: (variables: Variables) => mutation.mutate(variables),
+  };
+}
