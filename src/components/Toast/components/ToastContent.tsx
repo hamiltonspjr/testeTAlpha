45489e7020e91339746f0648e61852c0f9e4e1a5
hamilton <hamilton-souza@outlook.com.br>
2024-08-08 17:26:31 -0300

    feat(toast): criado componente com o conteúdo do toast

---
+++ src/components/Toast/components/ToastContent.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import {Dimensions} from 'react-native';
+import {Toast, ToastType} from '../../../services/toast/toastType';
+import {Box, BoxProps} from '../../Box/Box';
+import {Icon, IconProps} from '../../Icon/Icon';
+import {Text} from '../../Text/Text';
+import {$shadowProps} from '../../../theme/theme';
+
+const MAX_WIDTH = Dimensions.get('screen').width * 0.9;
+
+interface Props {
+  toast: Toast;
+}
+
+export function ToastContent({toast}: Props) {
+  const type: ToastType = toast.type || 'success';
+
+  return (
+    <Box {...$boxStyle} style={$shadowProps}>
+      <Icon {...mapTypeToIcon[type]} />
+      <Text style={{flexShrink: 1}} ml="s16" preset="paragraphMedium" bold>
+        {toast?.message}
+      </Text>
+    </Box>
+  );
+}
+
+const mapTypeToIcon: Record<ToastType, IconProps> = {
+  success: {
+    color: 'success',
+    name: 'checkRound',
+  },
+  error: {
+    color: 'error',
+    name: 'errorRound',
+  },
+};
+
+const $boxStyle: BoxProps = {
+  backgroundColor: 'background',
+  padding: 's16',
+  flexDirection: 'row',
+  borderRadius: 's16',
+  alignItems: 'center',
+  opacity: 0.95,
+  maxWidth: MAX_WIDTH,
+};
