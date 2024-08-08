4eaf35c62b4293351af80b07341487d8296b84c4
hamilton <hamilton-souza@outlook.com.br>
2024-08-07 22:22:55 -0300

    feat(navigation): adicionado componente de routes

--- App.tsx
+++ App.tsx
@@ -2,16 +2,14 @@ import {ThemeProvider} from '@shopify/restyle';
 import React from 'react';
 
 import {theme} from './src/theme/theme';
-import {Text} from './src/components/Text/Text';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
+import {Router} from './src/routes/Routes';
 
 function App(): React.JSX.Element {
   return (
     <SafeAreaProvider>
       <ThemeProvider theme={theme}>
-        <Text preset="headingLarge" color="carrotSecondary">
-          Teste
-        </Text>
+        <Router />
       </ThemeProvider>
     </SafeAreaProvider>
   );
