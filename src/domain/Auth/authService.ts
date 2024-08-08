3eefbbbc419ac9be5940fe9d0095872fe2793aa4
hamilton <hamilton-souza@outlook.com.br>
2024-08-08 17:30:15 -0300

    feat(auth): criado arquivo da camada de service utilizada na arquitetura do app onde vai pegar os dados que são retornados pela camada de api e adaptar para ser utilizado

---
+++ src/domain/Auth/authService.ts
@@ -0,0 +1,37 @@
+import {api} from '../../api/apiConfig';
+import {authAdapter} from './authAdapter';
+import {authApi} from './authApi';
+import {AuthCredentials, SignUpData, SignUpDataApiReturn} from './authTypes';
+
+async function signIn(
+  taxNumber: string,
+  password: string,
+): Promise<AuthCredentials> {
+  try {
+    const authCredentialsAPI = await authApi.signIn(taxNumber, password);
+    return authAdapter.toAuthCredentials(authCredentialsAPI);
+  } catch (error) {
+    throw new Error('login ou senha inválido');
+  }
+}
+
+async function signUp(
+  signUpData: SignUpData,
+): Promise<SignUpDataApiReturn | void> {
+  try {
+    await authApi.signUp(signUpData);
+  } catch (error) {
+    throw new Error('Usuário já cadastrado');
+  }
+}
+
+function updateToken(token: string) {
+  console.log(token);
+  api.defaults.headers.Authorization = `Bearer ${token}`;
+}
+
+export const authService = {
+  signIn,
+  signUp,
+  updateToken,
+};
