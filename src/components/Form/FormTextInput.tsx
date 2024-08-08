c558014f4fad09471ce7090030a6a1cafedabb4f
hamilton <hamilton-souza@outlook.com.br>
2024-08-08 10:16:41 -0300

    feat(textInput): componente de text input conectado ao react hook form

---
+++ src/components/Form/FormTextInput.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import {Controller, FieldValues, UseControllerProps} from 'react-hook-form';
+import {TextInput, TextInputProps} from '../TextInput/TextInput';
+
+export function FormTextInput<FormType extends FieldValues>({
+  control,
+  name,
+  rules,
+  errorMessage,
+  ...textInputProps
+}: TextInputProps & UseControllerProps<FormType>) {
+  return (
+    <Controller
+      control={control}
+      name={name}
+      rules={rules}
+      render={({field, fieldState}) => (
+        <TextInput
+          value={field.value}
+          onChangeText={field.onChange}
+          errorMessage={fieldState.error?.message || errorMessage}
+          {...textInputProps}
+        />
+      )}
+    />
+  );
+}
