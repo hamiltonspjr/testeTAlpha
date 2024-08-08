80189ce249c3c8d6818111deacfed4fc96194be7
hamilton <hamilton-souza@outlook.com.br>
2024-08-08 17:35:32 -0300

    feat(navigationTypes): adicionado type da stack de navegação do app

--- src/routes/navigationTypes.ts
+++ src/routes/navigationTypes.ts
@@ -1,11 +1,15 @@
 import {NativeStackScreenProps} from '@react-navigation/native-stack';
 import {AuthStackParamList} from './AuthStack';
+import {AppStackParamList} from './AppStack';
 
 declare global {
   namespace ReactNavigation {
-    interface RootParamList extends AuthStackParamList {}
+    interface RootParamList extends AppStackParamList, AuthStackParamList {}
   }
 }
 
 export type AuthScreenProps<RouteName extends keyof AuthStackParamList> =
   NativeStackScreenProps<AuthStackParamList, RouteName>;
+
+export type AppScreenProps<RouteName extends keyof AppStackParamList> =
+  NativeStackScreenProps<AppStackParamList, RouteName>;
