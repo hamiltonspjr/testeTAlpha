3e427d79c0f2f6f059a7156e244fb28096198342
hamilton <hamilton-souza@outlook.com.br>
2024-08-08 17:27:26 -0300

    feat(auth): arquivo que adapta os dados que retornam da api quando é feito login para o que será utilizado no app

---
+++ src/domain/Auth/authAdapter.ts
@@ -0,0 +1,11 @@
+import {AuthCredentials, AuthCredentialsAPI} from './authTypes';
+
+function toAuthCredentials(
+  authCredentialsAPI: AuthCredentialsAPI,
+): AuthCredentials {
+  return {
+    token: authCredentialsAPI.data.token,
+  };
+}
+
+export const authAdapter = {toAuthCredentials};
