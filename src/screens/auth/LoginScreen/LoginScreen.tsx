0c365aefe8fefca89ed5c63cb0ced0649ac9d7b4
hamilton <hamilton-souza@outlook.com.br>
2024-08-08 10:18:26 -0300

    feat(form): adicionado gerenciamento de estado ao formulário da screen e componentes de input conectados a lib de gerenciamento

--- src/screens/auth/LoginScreen/LoginScreen.tsx
+++ src/screens/auth/LoginScreen/LoginScreen.tsx
@@ -5,8 +5,26 @@ import {TextInput} from '../../../components/TextInput/TextInput';
 import {PasswordInput} from '../../../components/PasswordInput/PasswordInput';
 import {Button} from '../../../components/Button/Button';
 import {AuthScreenProps} from '../../../routes/navigationTypes';
+import {useForm} from 'react-hook-form';
+import {loginSchema, LoginSchemaType} from './loginSchema';
+import {zodResolver} from '@hookform/resolvers/zod';
+import {FormTextInput} from '../../../components/Form/FormTextInput';
+import {FormPasswordInput} from '../../../components/Form/FormPasswordInput';
 
 export function LoginScreen({navigation}: AuthScreenProps<'LoginScreen'>) {
+  const {control, formState, handleSubmit} = useForm<LoginSchemaType>({
+    resolver: zodResolver(loginSchema),
+    defaultValues: {
+      taxNumber: '',
+      password: '',
+    },
+    mode: 'onChange',
+  });
+
+  function submitForm({taxNumber, password}: LoginSchemaType) {
+    // TODO: implementar
+  }
+
   function navigateToSignUpScreen() {
     navigation.navigate('SignUpScreen');
   }
@@ -18,14 +36,26 @@ export function LoginScreen({navigation}: AuthScreenProps<'LoginScreen'>) {
       <Text preset="paragraphLarge" mb="s40">
         Digite seu login e senha para entrar
       </Text>
-      <TextInput
+      <FormTextInput
+        control={control}
+        name="taxNumber"
         label="Login"
-        placeholder="CPF ou CNPJ"
+        placeholder="Digite seu CPF ou CNPJ"
         boxProps={{mb: 's10'}}
       />
-      <PasswordInput label="Senha" placeholder="Digite sua senha" />
+      <FormPasswordInput
+        control={control}
+        name="password"
+        label="Senha"
+        placeholder="Digite sua senha"
+      />
 
-      <Button mt="s48" title="Entrar" />
+      <Button
+        mt="s48"
+        title="Entrar"
+        onPress={handleSubmit(submitForm)}
+        disabled={!formState.isValid}
+      />
       <Button
         onPress={navigateToSignUpScreen}
         mt="s12"
