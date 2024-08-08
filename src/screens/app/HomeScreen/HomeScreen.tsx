2a491b85656c65744142e88bc76a8651fca809e0
hamilton <hamilton-souza@outlook.com.br>
2024-08-08 17:36:46 -0300

    feat(screen): criação base da screen home

---
+++ src/screens/app/HomeScreen/HomeScreen.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import {Screen} from '../../../components/Screen/Screen';
+import {Text} from '../../../components/Text/Text';
+
+export function HomeScreen() {
+  return (
+    <Screen>
+      <Text>teste</Text>
+    </Screen>
+  );
+}
