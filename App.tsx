715fdb61b425f47ba3946f8b09da47784aea88a1
hamilton <hamilton-souza@outlook.com.br>
2024-08-08 17:22:42 -0300

    feat(providers): adicionado providers

--- App.tsx
+++ App.tsx
@@ -4,14 +4,24 @@ import React from 'react';
 import {theme} from './src/theme/theme';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
 import {Router} from './src/routes/Routes';
+import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
+import {AuthCredentialsProvider} from './src/services/authCredentials/Providers/AuthCredentialsProvider';
+import {Toast} from './src/components/Toast/Toast';
+
+const queryClient = new QueryClient();
 
 function App(): React.JSX.Element {
   return (
-    <SafeAreaProvider>
-      <ThemeProvider theme={theme}>
-        <Router />
-      </ThemeProvider>
-    </SafeAreaProvider>
+    <AuthCredentialsProvider>
+      <QueryClientProvider client={queryClient}>
+        <SafeAreaProvider>
+          <ThemeProvider theme={theme}>
+            <Router />
+            <Toast />
+          </ThemeProvider>
+        </SafeAreaProvider>
+      </QueryClientProvider>
+    </AuthCredentialsProvider>
   );
 }
 
