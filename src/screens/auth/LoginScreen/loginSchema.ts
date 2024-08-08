2bc8dc41be81b940a1e75a6bf60c977e6adfc2e7
hamilton <hamilton-souza@outlook.com.br>
2024-08-08 17:38:06 -0300

    feat(schema): modificado a validação da senha para o mínimo ser 6 caracteres

--- src/screens/auth/LoginScreen/loginSchema.ts
+++ src/screens/auth/LoginScreen/loginSchema.ts
@@ -3,7 +3,7 @@ import {cpfAndCnpjRegex} from '../../../utils/regexValidationSchema';
 
 export const loginSchema = z.object({
   taxNumber: z.string().regex(cpfAndCnpjRegex, 'cpf ou cnpj inválido'),
-  password: z.string().min(8, 'Senha deve ter no mínimo 8 caracteres'),
+  password: z.string().min(6, 'Senha deve ter no mínimo 6 caracteres'),
 });
 
 export type LoginSchemaType = z.infer<typeof loginSchema>;
