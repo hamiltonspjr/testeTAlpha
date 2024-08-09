3f8c7b385d17fd39552652938f783ed69e51f7db
hamilton <hamilton-souza@outlook.com.br>
2024-08-09 17:07:31 -0300

    feat(navigation): adicionado função para navegar para a página do produto clicado

--- src/components/ProductItem/ProductItem.tsx
+++ src/components/ProductItem/ProductItem.tsx
@@ -3,14 +3,23 @@ import {Product} from '../../domain/Products/productTypes';
 import {Box} from '../Box/Box';
 import {Text} from '../Text/Text';
 import {Pressable} from 'react-native';
+import {useNavigation} from '@react-navigation/native';
 
 interface Props {
   product: Product;
 }
 
 export function ProductItem({product}: Props) {
+  const navigation = useNavigation();
+
+  function goProductPage() {
+    navigation.navigate('ProductScreen', {
+      product,
+    });
+  }
+
   return (
-    <Pressable>
+    <Pressable onPress={goProductPage}>
       <Box
         backgroundColor="gray4"
         marginBottom="s24"
