c4014f53176ab3597cdcb0f4a708f9136e537fa8
hamilton <hamilton-souza@outlook.com.br>
2024-08-09 13:07:02 -0300

    feat(HomeScreen): testando o retorno da lista de produtos

--- src/screens/app/HomeScreen/HomeScreen.tsx
+++ src/screens/app/HomeScreen/HomeScreen.tsx
@@ -1,11 +1,35 @@
 import React from 'react';
 import {Screen} from '../../../components/Screen/Screen';
 import {Text} from '../../../components/Text/Text';
+import {useProductList} from '../../../domain/Products/useCases/useProductList';
+import {FlatList, ListRenderItemInfo} from 'react-native';
+import {Product} from '../../../domain/Products/productTypes';
+import {Box} from '../../../components/Box/Box';
+import {useScrollToTop} from '@react-navigation/native';
 
 export function HomeScreen() {
+  const {productList} = useProductList();
+
+  const flatListRef = React.useRef<FlatList<Product>>(null);
+  useScrollToTop(flatListRef);
+
+  function renderProduct({item}: ListRenderItemInfo<Product>) {
+    return (
+      <Box>
+        <Text>{item.name}</Text>
+      </Box>
+    );
+  }
+
   return (
     <Screen>
-      <Text>teste</Text>
+      <FlatList
+        data={productList}
+        ref={flatListRef}
+        keyExtractor={item => item.id.toString()}
+        showsVerticalScrollIndicator={false}
+        renderItem={renderProduct}
+      />
     </Screen>
   );
 }
