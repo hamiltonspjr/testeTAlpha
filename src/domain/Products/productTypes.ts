6265649a134f2d545c60693d6950adf48ce1b3bb
hamilton <hamilton-souza@outlook.com.br>
2024-08-09 13:05:35 -0300

    feat(product): arquivo de definição de types do produto

---
+++ src/domain/Products/productTypes.ts
@@ -0,0 +1,15 @@
+export interface ProductApi {
+  id: number;
+  name: string;
+  description: string;
+  price: number;
+  stock: number;
+}
+
+export interface Product {
+  id: number;
+  name: string;
+  description: string;
+  price: number;
+  stock: number;
+}
